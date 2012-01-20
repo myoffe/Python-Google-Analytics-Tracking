1ef86d775d93001cd6888095dd26f9d98fddda06
Michael Yoffe <myoffe@gmail.com>
2012-01-20 03:26:57 +0200

    replace php http request building with urllib + urllib2

--- src/analytics/internals/requests/HttpRequest.py
+++ src/analytics/internals/requests/HttpRequest.py
@@ -25,172 +25,121 @@ Google Analytics is a registered trademark of Google Inc.
 @author    Thomas Bachem <tb@unitedprototype.com>
 @copyright Copyright (c) 2010 United Prototype GmbH (http://unitedprototype.com)
 """
+import urllib
+import urllib2
 
 from analytics import Config
 from analytics.internals import utils
 
 
-""" 
-@link http://code.google.com/p/gaforflash/source/browse/trunk/src/com/google/analytics/core/GIFRequest.as
-"""
+
+# @link http://code.google.com/p/gaforflash/source/browse/trunk/src/com/google/analytics/core/GIFRequest.as
 class HttpRequest(object):
 
-    def __init__(self, self, config=None):
+    def __init__(self, config=None):
         # Indicates the type of request, will be mapped to "utmt" parameter
         # @see ParameterHolder::utmt
         # @var string
         self.type = None
 
         self.config = None
-        self.xForwardedFor = None
+        self.x_forwarded_for = None
         self.userAgent = None
 
         self.setConfig(config or Config())
     
 
-    """ 
-    @return \UnitedPrototype\GoogleAnalytics\Config
-    """
     def getConfig(self):
         return self.config
 
 
-    """ 
-    @param \UnitedPrototype\GoogleAnalytics\Config config
-    """
     def setConfig(self, config):
         self.config = config
 
 
-    """ 
-    @param string value
-    """
-    def setXForwardedFor(self, value):
-        self.xForwardedFor = value
+    def setx_forwarded_for(self, value):
+        self.x_forwarded_for = value
 
 
-    """ 
-    @param string value
-    """
     def setUserAgent(self, value):
         self.userAgent = value
 
-
-    """ 
-    @return string
-    """
     def buildHttpRequest(self):
-        parameters = self.buildParameters()
-
-        # This constant is supported as the 4th argument of http_build_query()
-        # from PHP 5.3.6 on and will tell it to use rawurlencode() instead of urlencode()
-        # internally, see http://code.google.com/p/php-ga/issues/detail?id=3
-        if(defined('PHP_QUERY_RFC3986')) 
-            # http_build_query() does automatically skip all array entries
-            # with None values, exactly what we want here
-            queryString = http_build_query(parameters.toArray(), '', '&', PHP_QUERY_RFC3986)
-         else:
-            # Manually replace "+"s with "%20" for backwards-compatibility
-            queryString = str_replace('+', '%20', http_build_query(parameters.toArray(), '', '&'))
+        parameters = self.build_parameters()
+        query_string = urllib.urlencode(parameters)
+        url = self.config.get_endpoint_host() + self.config.get_endpoint_path()
 
         # Mimic Javascript's encodeURIComponent() encoding for the query
         # string just to be sure we are 100% consistent with GA's Javascript client
-        queryString = Util::convertToUriComponentEncoding(queryString)
+        query_string = utils.convert_to_uri_component_encoding(query_string)
 
         # Recent versions of ga.js use HTTP POST requests if the query string is too long
-        usePost = strlen(queryString) > 2036
+        usePost = len(query_string) > 2036
 
-        if( not usePost) 
-            r = 'GET ' . self.config.getEndpointPath() . '?' . queryString . ' HTTP/1.0' . "\r\n"
-         else:
-            # FIXME: The "/p" shouldn't be hardcoded here, instead we need a GET and a POST endpoint...
-            r = 'POST /p' . self.config.getEndpointPath() . ' HTTP/1.0' . "\r\n"
+        if not usePost:
+            req = urllib2.Request(url + '?' + query_string)
+        else:
+            req = urllib2.Request(url, data=query_string)
 
-        r .= 'Host: ' . self.config.getEndpointHost() . "\r\n"
+        if self.userAgent:
+            req.add_header('User-Agent', self.userAgent.replace('\n','').replace('\r',''))
 
-        if(self.userAgent) 
-            r .= 'User-Agent: ' . str_replace(array("\n", "\r"), '', self.userAgent) . "\r\n"
-
-
-        if(self.xForwardedFor) 
-            # Sadly "X-Fowarded-For" is not supported by GA so far,
+        if self.x_forwarded_for:
+            # Sadly "X-Forwarded-For" is not supported by GA so far,
             # see e.g. http://www.google.com/support/forum/p/Google+Analytics/thread?tid=017691c9e71d4b24,
             # but we include it nonetheless for the pure sake of correctness (and hope)
-            r .= 'X-Forwarded-For: ' . str_replace(array("\n", "\r"), '', self.xForwardedFor) . "\r\n"
+            # TODO do I need the \r\n replaces?
+            req.add_header('X-Forwarded-For', self.x_forwarded_for.replace('\n','').replace('\r',''))
 
-
-        if(usePost) 
+        if usePost:
             # Don't ask me why "text/plain", but ga.js says so :)
-            r .= 'Content-Type: text/plain' . "\r\n"
-            r .= 'Content-Length: ' . strlen(queryString) . "\r\n"
+            req.add_header('Content-Type', 'text/plain')
+            req.add_header('Content-Length', str(len(query_string)))
+
+        return req
 
 
-        r .= 'Connection: close' . "\r\n"
-        r .= "\r\n\r\n"
-
-        if(usePost) 
-            r .= queryString
+    def build_parameters(self):
+        raise NotImplementedError()
 
 
-        return r
+    def _send(self):
+        """
+        This method should only be called directly or indirectly by fire(), but must
+        remain public as it can be called by a closure function.
 
+        Sends either a normal HTTP request with response or an asynchronous request
+        to Google Analytics without waiting for the response. Will always return
+        null in the latter case, or false if any connection problems arise.
 
-    """ 
-    @return \UnitedPrototype\GoogleAnalytics\Internals\ParameterHolder
-    """
-    def buildParameters(self):
-        request = self.buildHttpRequest()
+        @return null|string|bool
+        """
+        request = self.build_http_request()
         response = None
 
         # Do not actually send the request if endpoint host is set to None
-        if self.config.getEndpointHost():
-            timeout = self.config.getRequestTimeout()
-            
-            socket = fsockopen(self.config.getEndpointHost(), 80, errno, errstr, timeout)
-            if( not socket) return False
-            
-            if(self.config.getFireAndForget()) 
-                stream_set_blocking(socket, False)
-            
-            
-            timeoutS  = intval(timeout)
-            timeoutUs = (timeout - timeoutS)  100000
-            stream_set_timeout(socket, timeoutS, timeoutUs)
-            
-            fwrite(socket, request)
-            
-            if( not self.config.getFireAndForget()) 
-                while( not feof(socket)) 
-                    response .= fgets(socket, 512)
-                
-            
-            
-            fclose(socket)
+        if self.config.get_endpoint_host():
+            timeout = self.config.request_timeout
 
+            if self.config.getFireAndForget():
+                raise NotImplementedError('Asynchronous send not implemented yet')
 
-        if(loggingCallback = self.config.getLoggingCallback()) 
-            loggingCallback(request, response)
+            # TODO Handle errors?
+            response = urllib2.urlopen(request, timeout=timeout).read()
+
+        logging_callback = self.config.logging_callback
+        if logging_callback:
+            logging_callback(request, response)
 
 
         return response
 
 
-    """ 
-    Simply delegates to send() if config option "sendOnShutdown" is disabled
-    or enqueues the request by registering a PHP shutdown function.
-    """
     def fire(self):
-        if(self.config.getSendOnShutdown()) 
-            # This dumb variable assignment is needed as PHP prohibits using
-            # this in closure use statements
-            instance = this
-            # We use a closure here to retain the current values/states of
-            # this instance and request (as the use statement will copy them
-            # into its own scope)
-            register_shutdown_function(function() use(instance) 
-                instance._send()
-            )
-         else:
+        if self.config.send_on_shutdown:
+            import atexit
+            atexit.register(lambda: self._send())
+        else:
             self._send()
 
 
